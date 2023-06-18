9de9b34112e19bd78fbba70e94357ce841b20e79
Maw Paing Thu <paingmawthu@gmail.com>
2023-06-19 01:54:15 +0900

    Modified About us page

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -23,14 +23,14 @@
         </div>
         <div class="member-info content py-5">
             <div class="container bg-white py-3 px-3">
-                <div class="inner-content greeting text-center bg-white p-3 px-md-5">
+                <div class="inner-content greeting text-center bg-white p-3 pb-5 px-md-5 border-bottom">
                     <h4 class="rank py-3">{{ site_info.greeting.title[lang] }}</h4>
                     <span class="speech" v-html="site_info.greeting.speech[lang]"></span>
                 </div>
                 <div class="row m-0 g-5">
-                    <div class="col-12 col-md-4 p-3" :class="(index+1)%3 ?'border--md-end ':''" v-for="(member,index) in member_info" :key="member.id">
+                    <div class="col-12 col-md-4 p-3 border-bottom" :class="(index+1)%3 ?'border--md-end ':''" v-for="(member,index) in member_info" :key="member.id">
                         <div class="inner-content text-center">
-                            <img :src="member.img" alt="" class="rounded-circle">
+                            <img :src="member.img" alt="" class="rounded-circle shadow">
                             <span class="name pt-3">{{ member.name[lang] }}</span>
                             <span class="remark">{{ member.remark[lang] }}</span>
                             <span class="speech pt-3">{{ member.speech[lang] }}</span>
