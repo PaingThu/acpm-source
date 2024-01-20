356376b93b97a03a97526be51589b93dace72725
Maw Paing Thu <paingmawthu@gmail.com>
2024-01-20 10:49:26 +0900

    Renring Google Drive Image URL cannot show image So I fixed the error with other rendring url old -> https://drive.google.com/uc?id={image_id} fixed -> https://lh3.google.com/u/0/d/{image_id}

--- src/contents/CurrentActivitiesContent.vue
+++ src/contents/CurrentActivitiesContent.vue
@@ -17,7 +17,8 @@
                     <template v-for="(event, eindex) in eventInfo.new" :key="eindex">
                         <div class="each-new-event col-12 col-md-3 border-bottom">
                             <div class="banner d-flex align-items-center shadow">
-                                <img :src="`https://drive.google.com/uc?id=${event.image_id}`" alt="" class="w-100">
+                                <!-- <img :src="`https://drive.google.com/uc?id=${event.image_id}`" alt="" class="w-100"> -->
+                                <img :src="`https://lh3.google.com/u/0/d/${event.image_id}`" alt="" class="w-100">
                             </div>
                             <div class="py-3 d-flex flex-column gap-1">
                                 <span>{{ event.date }}</span>
@@ -34,7 +35,8 @@
                     <template v-for="(event, eindex) in eventInfo.old" :key="eindex">
                         <div class="each-old-event w-100 border-bottom pb-3">
                             <div class="banner d-flex align-items-center shadow">
-                                <img :src="`https://drive.google.com/uc?id=${event.image_id}`" alt="" class="w-100">
+                                <!-- <img :src="`https://drive.google.com/uc?id=${event.image_id}`" alt="" class="w-100"> -->
+                                <img :src="`https://lh3.google.com/u/0/d/${event.image_id}`" alt="" class="w-100">
                             </div>
                             <div class="info d-flex flex-column gap-2 py-2">
                                 <span>{{ event.date }}</span>
